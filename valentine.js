89e5094466cf514616ca68f7f139379db74730b1
SaiBhargav2k1 <110883557+SaiBhargav2k1@users.noreply.github.com>
2025-02-08 20:57:42 +0530

    Add files via upload

---
+++ valentine.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const message = document.querySelector('.message');
+  message.addEventListener('click', () => {
+    alert('I love you to the moon and back! 🌙❤️');
+  });
+});
